047af20afa7f5d55016955aeabe22462a416eebf
Vik <kalnoy.v@yandex.ru>
2021-05-19 00:04:07 +0300

    Initial Commit

---
+++ Shared/CoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoAppApp.swift
+//  Shared
+//
+//  Created by Vik on 19.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
